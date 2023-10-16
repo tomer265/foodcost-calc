bb7bea2bbfb29b51f4617b6f866253f4da6e87e3
Tomer Lavi <tomer265@hotmail.com>
2023-10-16 22:03:11 +0300

    added sass

--- electron-vue-template-master/src/renderer/components/Login.vue
+++ electron-vue-template-master/src/renderer/components/Login.vue
@@ -7,15 +7,54 @@ const store = useIsLoggedInStore();
 const loginHandler = () => {
   store.loginSuccess();
 };
+
+const emailRules = [
+  (v) =>
+    v.match(
+      /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
+    ) || "Invalid Email address",
+];
 </script>
 
 <template>
   <v-form>
-    <v-text-field
-      label="Email"
-      prepend-icon="fa:fas fa-lock"
-      variant="solo-filled"
-    ></v-text-field>
+    <div class="form-line">
+      <v-responsive class="mx-auto" max-width="344">
+        <v-text-field
+          label="Email"
+          hint="Enter your email"
+          :rules="emailRules"
+          variant="solo-filled"
+          clearable
+          prepend-inner-icon="mdi-email-outline"
+        ></v-text-field>
+      </v-responsive>
+      <div class="form-line">
+        <v-responsive class="mx-auto" max-width="344">
+          <v-text-field
+            label="Password"
+            hint="Enter your password"
+            variant="solo-filled"
+            clearable
+            prepend-inner-icon="mdi-key-variant"
+          ></v-text-field>
+        </v-responsive>
+      </div>
+    </div>
+
     <button @click="loginHandler">Login</button>
   </v-form>
 </template>
+
+<style lang="scss" scoped>
+form {
+  padding: 0;
+  width: 50vw;
+  margin: auto;
+
+  .form-line {
+    justify-content: center;
+    align-items: center;
+  }
+}
+</style>
